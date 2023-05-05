2a3332b639ab625c2b8bce587555e800d245ef2f
Sam Wright <samwright@Sams-MacBook-Pro-3.local>
2023-05-04 20:25:00 -0500

    Built index files to import and export modules

--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,12 @@
-//index.js
+// Require Express
+// Importing the api routes 
+const router = require('express').Router();
+const apiRoutes = require('./api');
+
+// Making the api routes module accessible 
+router.use('/api', apiRoutes);
+
+router.use((req, res) => res.send('Incorrect route'));
+
+// Exporting router
+module.exports = router;
