1dc47633273f855b9193e22001feae037240ecac
crmiller2010 <114351462+crmiller2010@users.noreply.github.com>
2023-05-15 01:13:50 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 const express = require ('express');
 const cors = require ('cors');
-require('dotenv').config();
+
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
-const port = process.env.PORT || 5000;
+const port =  5000;
 const connectDB = require('./config/connections');
 
 const app = express();
@@ -14,7 +14,7 @@ connectDB();
 
 app.use('/graphql', graphqlHTTP({
     schema,
-    graphiql: process.env.NODE_ENV === 'development'
+    
 }))
 
 app.listen(port, console.log(`Server running on port ${port}`));
